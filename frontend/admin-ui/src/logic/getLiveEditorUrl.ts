5ab6d16f20898c348ee994c6bf14413482fcb86b
denis <dskulovich@gmail.com>
2023-06-15 09:03:56 +0300

    modified json view

--- frontend/admin-ui/src/logic/getLiveEditorUrl.ts
+++ frontend/admin-ui/src/logic/getLiveEditorUrl.ts
@@ -1,13 +1,13 @@
 const getLiveEditorUrl = (env: "dev" | "prod") => {
-    const LIVE_EDITOR_DEV: string | undefined = import.meta.env.LIVE_EDITOR_DEV
+    const VITE_APP_LIVE_EDITOR_DEV: string | undefined = import.meta.env.VITE_APP_LIVE_EDITOR_DEV
     const LIVE_EDITOR_PROD: string | undefined = import.meta.env.LIVE_EDITOR_PROD
 
     let url: string
     if (env !== "prod") {
-        if (!LIVE_EDITOR_DEV) throw new Error("LIVE_EDITOR_DEV is a mandatory env param")
-        url = LIVE_EDITOR_DEV
+        if (!VITE_APP_LIVE_EDITOR_DEV) throw new Error("VITE_APP_LIVE_EDITOR_DEV is a mandatory env param")
+        url = VITE_APP_LIVE_EDITOR_DEV
     } else {
-        if (!LIVE_EDITOR_PROD) throw new Error("LIVE_EDITOR_DEV is a mandatory env param")
+        if (!LIVE_EDITOR_PROD) throw new Error("VITE_APP_LIVE_EDITOR_DEV is a mandatory env param")
         url = LIVE_EDITOR_PROD
     }
     return url
